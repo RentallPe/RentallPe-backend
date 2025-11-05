6bfe28aac2a30032e16f423b32075e591e5c43d1
Thunder <artax1605@gmail.com>
2025-11-05 00:46:02 -0500

    feat: template of rentalPeAPI

---
+++ RentalPeAPI/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,41 @@
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentalPeAPI.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+/// <summary>
+/// Represents the application's database context using Entity Framework Core.
+/// </summary>
+/// <param name="options">The options for configuring the context.</param>
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    /// <summary>
+    /// Configures the database context options.
+    /// </summary>
+    /// <param name="builder">The options' builder.</param>
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        // Add the created and updated interceptor
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+
+    /// <summary>
+    /// Configures the model for the database context.
+    /// </summary>
+    /// <param name="builder">The model builder.</param>
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Publishing Context
+      //builder.ApplyPublishingConfiguration();
+
+      //// Profiles Context
+      //builder.ApplyProfilesConfiguration();
+        
+        // Use snake case for database objects and pluralization for table names
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
---
+++ RentalPeAPI/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using RentalPeAPI.Shared.Domain.Repositories;
+using RentalPeAPI.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+/// <summary>
+/// Implementation of the unit of work pattern using Entity Framework Core.
+/// </summary>
+/// <param name="context">The database context.</param>
+public class UnitOfWork(AppDbContext context): IUnitOfWork
+{
+    /// <summary>
+    /// Asynchronously saves all changes made in this unit of work.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
